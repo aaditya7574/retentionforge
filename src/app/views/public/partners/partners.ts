34f59970dd9eda0e8c01ac44ec7324d4a671ff63
Aaditya7574 <adityapanchal7574@gmail.com>
2026-01-06 22:50:20 +0530

    added scroll to top and footer

--- src/app/views/public/partners/partners.ts
+++ src/app/views/public/partners/partners.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-partners',
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './partners.html',
   styleUrl: './partners.scss',
 })
-export class Partners {
+export class Partners implements OnInit {
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 
 }
